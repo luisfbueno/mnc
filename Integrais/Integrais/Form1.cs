cd2ff30df67284c4022f543adb008eaa990795f9
Luis Felipe <luis.bueno96@gmail.com>
2016-09-17 16:12:08 -0300

    1/3 de Simpson funcionando para subintervalos impares

--- Integrais/Integrais/Form1.cs
+++ Integrais/Integrais/Form1.cs
@@ -20,6 +20,14 @@ namespace Integrais
         public Form1()
         {
             InitializeComponent();
+
+            /*fx.Text = "sin(x)";
+            valA.Text = "0";
+            valB.Text = "1,0472";
+            valN.Text = "5";
+            tercoSimpson.Checked = true;*/
+
+            quadGauss.Enabled = false;
         }
 
         private void MetodoRetanguloEsquerda()
@@ -73,17 +81,21 @@ namespace Integrais
 
             parser.Values.Add("x", a);
             double fzero = parser.Parse(func);
+            MessageBox.Show("Fzero = " + fzero);
 
             double somatorio = 0;
             while (x < b)
             {
                 parser.Values["x"].SetValue(x);
                 somatorio += parser.Parse(func);
+                MessageBox.Show(parser.Parse(func).ToString());
                 x += h;
             }
 
             parser.Values["x"].SetValue(b);
             double fn = parser.Parse(func);
+
+            MessageBox.Show("Fzero = " + fn);
 
             double resu = (h / 2) * (fzero + fn + 2 * somatorio);
 
@@ -135,17 +147,84 @@ namespace Integrais
 
             else //se n for impar, de 0 a n-1 aplicar 1/3 de simpson e de n-1 a n trapézios
             {
-                //Simpson -> 0 ate m / Trapezio -> m até n
+                //Simpson -> 0 ate m(n-1) / Trapezio -> m até n
 
+                double h = (b - a) / n; //divide o intervalo maior em pedaços
+                double m = a + (n - 1) * h; //salva o penultimo pedaço
+                //Faz o Simpson de a até m                
 
+                parser.Values.Add("x", a);
+                double fzero = parser.Parse(func); //calcula f0
+                double x = a + h;
 
+                double somatorioPar = 0;
+                double somatorioImpar = 0;
+
+                for (int i = 1;i<n-1;i++)
+                {
+                    parser.Values["x"].SetValue(x);
+
+                    if (i % 2 == 0)
+                    {
+                        somatorioPar += parser.Parse(func);
+                    }
+
+                    else
+                    {
+                        somatorioImpar += parser.Parse(func);
+                    }
+
+                    x += h;
+
+                }
+
+               // MessageBox.Show(somatorioImpar.ToString());
+               // MessageBox.Show(somatorioPar.ToString());
+
+                parser.Values["x"].SetValue(m);
+                double fn = parser.Parse(func);
+
+                double resuSimpson = (h / 3) * (fzero + fn + (4 * somatorioImpar) + (2 * somatorioPar));
+
+                //Trapézio
+                parser.Values["x"].SetValue(m);
+                double fm = parser.Parse(func);
+                parser.Values["x"].SetValue(b);
+                double fb = parser.Parse(func);
+
+                double resuTrapezio = (h / 2) * (fm + fb);
+
+                double resuFinal = resuTrapezio + resuSimpson;
+                solucao.Text = resuFinal.ToString();
 
 
             }
     
 
         }
+//------------------------------------------------------------------
+        private void MetodoOitavoSimpson()
+        {
+            ExpressionParser parser = new ExpressionParser();
 
+            if(n%3 == 0)
+            {
+                double h = (b - a) / 2;
+
+                parser.Values.Add("x", a);
+                double fzero = parser.Parse(func);
+                double x = a + h;
+
+                
+
+
+
+            }
+
+
+        }
+
+//FUNÇÕES DE INTERFACE
         private bool PassaTextDouble()
         {
 
