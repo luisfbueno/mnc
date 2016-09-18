f2120f351a33c8b750d3ca7af4a7d3135f4e35c5
Luis Felipe <luis.bueno96@gmail.com>
2016-09-18 16:27:50 -0300

    Interface, calculo de derivadas parciais e sistemas adicionado ao programa de Sistemas não-lineares

--- Sistemas não-lineares/Sistemas não-lineares/Form1.Designer.cs
+++ Sistemas não-lineares/Sistemas não-lineares/Form1.Designer.cs
@@ -28,12 +28,319 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ordemSist = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.newtonMod = new System.Windows.Forms.RadioButton();
+            this.newton = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.epsilon = new System.Windows.Forms.TextBox();
+            this.iteracoes = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.calc = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.tabelaPontos = new System.Windows.Forms.TableLayoutPanel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ordemSist)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iteracoes)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.tabelaPontos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.ordemSist);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(131, 86);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ordem do sistema";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "2 <= n <= 10";
+            // 
+            // ordemSist
+            // 
+            this.ordemSist.Location = new System.Drawing.Point(9, 53);
+            this.ordemSist.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.ordemSist.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.ordemSist.Name = "ordemSist";
+            this.ordemSist.Size = new System.Drawing.Size(64, 20);
+            this.ordemSist.TabIndex = 0;
+            this.ordemSist.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.ordemSist.ValueChanged += new System.EventHandler(this.ordemSist_ValueChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.newtonMod);
+            this.groupBox2.Controls.Add(this.newton);
+            this.groupBox2.Location = new System.Drawing.Point(150, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(140, 86);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Métodos";
+            // 
+            // newtonMod
+            // 
+            this.newtonMod.AutoSize = true;
+            this.newtonMod.Location = new System.Drawing.Point(6, 56);
+            this.newtonMod.Name = "newtonMod";
+            this.newtonMod.Size = new System.Drawing.Size(117, 17);
+            this.newtonMod.TabIndex = 1;
+            this.newtonMod.TabStop = true;
+            this.newtonMod.Text = "Newton Modificado";
+            this.newtonMod.UseVisualStyleBackColor = true;
+            // 
+            // newton
+            // 
+            this.newton.AutoSize = true;
+            this.newton.Location = new System.Drawing.Point(6, 24);
+            this.newton.Name = "newton";
+            this.newton.Size = new System.Drawing.Size(65, 17);
+            this.newton.TabIndex = 0;
+            this.newton.TabStop = true;
+            this.newton.Text = "Newton ";
+            this.newton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.epsilon);
+            this.groupBox3.Controls.Add(this.iteracoes);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(296, 13);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(238, 86);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Critérios de parada";
+            // 
+            // epsilon
+            // 
+            this.epsilon.Location = new System.Drawing.Point(172, 55);
+            this.epsilon.Name = "epsilon";
+            this.epsilon.Size = new System.Drawing.Size(60, 20);
+            this.epsilon.TabIndex = 3;
+            // 
+            // iteracoes
+            // 
+            this.iteracoes.Location = new System.Drawing.Point(172, 18);
+            this.iteracoes.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.iteracoes.Name = "iteracoes";
+            this.iteracoes.Size = new System.Drawing.Size(60, 20);
+            this.iteracoes.TabIndex = 2;
+            this.iteracoes.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "ε: (de 0,0001 a 0,01)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Iterações: (de 10 a 100)";
+            // 
+            // calc
+            // 
+            this.calc.Location = new System.Drawing.Point(540, 14);
+            this.calc.Name = "calc";
+            this.calc.Size = new System.Drawing.Size(85, 86);
+            this.calc.TabIndex = 3;
+            this.calc.Text = "Calcula";
+            this.calc.UseVisualStyleBackColor = true;
+            this.calc.Click += new System.EventHandler(this.calc_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.tabelaPontos);
+            this.groupBox4.Location = new System.Drawing.Point(13, 106);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(612, 283);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Pontos e equações";
+            // 
+            // tabelaPontos
+            // 
+            this.tabelaPontos.ColumnCount = 5;
+            this.tabelaPontos.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tabelaPontos.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tabelaPontos.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tabelaPontos.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tabelaPontos.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tabelaPontos.Controls.Add(this.label8, 4, 0);
+            this.tabelaPontos.Controls.Add(this.label7, 3, 0);
+            this.tabelaPontos.Controls.Add(this.label6, 2, 0);
+            this.tabelaPontos.Controls.Add(this.label4, 0, 0);
+            this.tabelaPontos.Controls.Add(this.label5, 1, 0);
+            this.tabelaPontos.Location = new System.Drawing.Point(9, 20);
+            this.tabelaPontos.Name = "tabelaPontos";
+            this.tabelaPontos.RowCount = 11;
+            this.tabelaPontos.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tabelaPontos.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9F));
+            this.tabelaPontos.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9F));
+            this.tabelaPontos.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9F));
+            this.tabelaPontos.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9F));
+            this.tabelaPontos.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9F));
+            this.tabelaPontos.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9F));
+            this.tabelaPontos.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9F));
+            this.tabelaPontos.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9F));
+            this.tabelaPontos.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9F));
+            this.tabelaPontos.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9F));
+            this.tabelaPontos.Size = new System.Drawing.Size(597, 258);
+            this.tabelaPontos.TabIndex = 0;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.SystemColors.Control;
+            this.label8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label8.Location = new System.Drawing.Point(478, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(116, 25);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "f[i](x) final";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.SystemColors.Control;
+            this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label7.Location = new System.Drawing.Point(359, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(113, 25);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "x[i] final";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.Control;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Location = new System.Drawing.Point(121, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(232, 25);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "f[i](x)";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 25);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "i";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(32, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 25);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "x[i] inicial";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(637, 392);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.calc);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Sistemas de equações não-lineares";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ordemSist)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iteracoes)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.tabelaPontos.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown ordemSist;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton newtonMod;
+        private System.Windows.Forms.RadioButton newton;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.NumericUpDown iteracoes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox epsilon;
+        private System.Windows.Forms.Button calc;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TableLayoutPanel tabelaPontos;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Sistemas não-lineares/Sistemas não-lineares/Form1.cs
+++ Sistemas não-lineares/Sistemas não-lineares/Form1.cs
@@ -7,14 +7,240 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using info.lundin.math;
 
 namespace Sistemas_não_lineares
 {
     public partial class Form1 : Form
     {
+        TextBox[] xi = new TextBox[10];
+        TextBox[] fxi = new TextBox[10];
+        TextBox[] xifinal = new TextBox[10];
+        TextBox[] fxifinal = new TextBox[10];
+        double[] vetX = new double[10];
+        string[] vetFx = new string[10];
+        double eps;
+        double it;
+        ExpressionParser parser = new ExpressionParser();
+
         public Form1()
         {
             InitializeComponent();
+
+            //Adiciona labels
+            Label[] nums = new Label[10];
+            for (int i =1; i <= 10; i++)
+            {
+                nums[i - 1] = new Label();
+                nums[i-1].Text = i.ToString();
+                nums[i - 1].AutoSize = false;
+                nums[i - 1].Dock = DockStyle.Fill;
+                nums[i - 1].TextAlign = ContentAlignment.MiddleCenter;
+                tabelaPontos.Controls.Add(nums[i - 1],0,i);
+                parser.Values.Add("x" + i, 0);
+            }
+
+            for (int i = 1; i <= 10; i++)
+            {
+                xi[i-1] = new TextBox();
+                xi[i - 1].Dock = DockStyle.Fill;
+                fxi[i - 1] = new TextBox();
+                fxi[i-1].Dock = DockStyle.Fill;
+                xifinal[i - 1] = new TextBox();
+                xifinal[i - 1].Dock = DockStyle.Fill;
+                fxifinal[i - 1] = new TextBox();
+                fxifinal[i - 1].Dock = DockStyle.Fill;
+
+                tabelaPontos.Controls.Add(xi[i-1], 1, i);
+                tabelaPontos.Controls.Add(fxi[i-1], 2, i);
+                tabelaPontos.Controls.Add(xifinal[i-1], 3, i);
+                tabelaPontos.Controls.Add(fxifinal[i-1], 4, i);
+            }
+
+            ordemSist.Value = 2;
+
+            fxi[0].Text = "(x1)^2 + (x2)^2 - 1";
+            fxi[1].Text = "(x1)*(x2)";
+            xi[0].Text = "0,5";
+            xi[1].Text = "0,1";
+            epsilon.Text = "0,01";
+
         }
+
+
+        private void gauss(int n, double[,] a, double[] b, ref double[] x)
+        {
+            int i, j, k;
+            double multiplicador, soma;
+
+            for (j = 0; j < n - 1; j++)
+            {
+                for (i = j + 1; i < n; i++)
+                {
+                    multiplicador = a[i, j] / a[j, j];
+                    for (k = j; k < n; k++)
+                    {
+                        a[i, k] -= multiplicador * a[j, k];
+                        MessageBox.Show("a[" + i.ToString() + "," + k.ToString() + "] = " + a[i, k]);
+                    }
+
+                    b[i] -= multiplicador * b[j];
+                    MessageBox.Show("b[" + i.ToString() + "] = " + b[i]);
+                }
+
+            }
+
+            x[n - 1] = b[n - 1] / a[n - 1, n - 1];
+            for (i = n - 2; i >= 0; i--)
+            {
+                soma = 0;
+                for (j = i + i; j < n; j++)
+                {
+                    soma += a[i, j] * x[j];
+                }
+                x[i] = (b[i] - soma) / a[i, i];
+            }
+
+        }
+//---------------------------------------------------------------------------------------------
+        double f(string f,double val,int ind)
+        {
+            parser.Values["x" + ind].SetValue(val);
+            return parser.Parse(f);
+        }
+//---------------------------------------------------------------------------------
+        double DerivadaParcial(string func,double[] x,int ind) //ind é o indice do vetor, tem q somar 1 quando usar no interpretador
+        {
+            double d = 0;
+            double h = 1000*0.001;
+            bool achou = false;
+            double p = 0, q;
+            double f1, f2, xaux;
+            int i;
+
+            xaux = x[ind];
+            x[ind] = xaux + h;
+            f1 = f(func, x[ind], ind + 1);
+            x[ind] = xaux - h;
+            f2 = f(func, x[ind], ind + 1);
+
+            p = (f1 - f2) / (2*h);
+
+            for (i = 0; i < 10 && !achou; i++)
+            {
+                q = p;
+                h /= 2;
+
+                x[ind] = xaux + h;
+                f1 = f(func, x[ind], ind + 1);
+                x[ind] = xaux - h;
+                f2 = f(func, x[ind], ind + 1);
+
+                p = (f1 - f2) / (2 * h);
+
+                if (Math.Abs(p - q) < 0.001)
+                    achou = true;
+
+            }
+
+            x[ind] = xaux;
+            parser.Values["x" + (ind + 1)].SetValue(x[ind]);
+            d = p;
+
+            return d;
+        }
+//---------------------------------------------------------------------------------
+        private void Newton(int n)
+        {
+            double[,] jacobiano = new double[10, 10];
+            double[] F = new double[10];
+            //double parada = 1;
+            //double cont = 1;
+            double[] x = new double[10];
+
+            for(int i = 0; i < n; i++)
+            {
+                x[i] = vetX[i];
+            }
+
+           // while(parada>eps && cont <= it)
+           // {
+
+                for(int i = 0; i < n; i++) //Calcula vetor F de valores das funções
+                {
+                    F[i] = parser.Parse(vetFx[i]);
+                MessageBox.Show("f[" + i +"] = " + F[i]);
+            }
+
+                for(int i = 0; i < n; i++) //Calcula matriz Jacobiana
+                {
+                    for(int j = 0; j < n; j++)
+                    {
+                        jacobiano[i, j] = DerivadaParcial(vetFx[i],vetX,j);
+                        MessageBox.Show("j[" + i + "," + j + "]=" + jacobiano[i, j]);
+                    }
+                }
+
+                //gauss(n, jacobiano, F, ref x);
+
+
+
+                //cont++;
+            //}
+
+        }
+
+        private void ordemSist_ValueChanged(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i <= ordemSist.Value)
+                {
+                    xi[i - 1].Enabled = true;
+                    fxi[i - 1].Enabled = true;
+                    xifinal[i - 1].Enabled = true;
+                    fxifinal[i - 1].Enabled = true;
+                }
+                else
+                {
+                    xi[i - 1].Enabled = false;
+                    fxi[i - 1].Enabled = false;
+                    xifinal[i - 1].Enabled = false;
+                    fxifinal[i - 1].Enabled = false;
+                }
+            }
+
+            
+        }
+//----------------------------------------------------------------------------------
+        private void calc_Click(object sender, EventArgs e)
+        {
+            int n = (int)ordemSist.Value;
+
+            for(int i = 0; i < n; i++)
+            {
+                if(xi[i].Text == "" || fxi[i].Text == "")
+                {
+                    MessageBox.Show("Preencha todos os espaços!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                vetX[i] = Double.Parse(xi[i].Text);
+                parser.Values["x" + (i + 1)].SetValue(vetX[i]);
+                vetFx[i] = fxi[i].Text;
+            }
+
+            eps = Double.Parse(epsilon.Text);
+            if(eps<0.0001 || eps > 0.01)
+            {
+                MessageBox.Show("Coloque um valor válido para epsilon!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            it = (int)iteracoes.Value;
+
+            Newton(n);
+        }
+
+
+
     }
 }
